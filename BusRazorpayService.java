6a19dfa087bb336383e27b3702e7b73358c4f8b0
Prapti Thambade <praptitambade313@gmail.com>
2025-08-21 13:38:02 +0530

    Add files via upload

---
+++ BusRazorpayService.java
@@ -0,0 +1,30 @@
+package com.Bus.Services;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+
+@Service
+public class BusRazorpayService {
+
+	@Value("${razorpay.key_id}")
+    private String razorpayKeyId;
+
+    @Value("${razorpay.key_secret}")
+    private String razorpayKeySecret;
+
+    public Order createOrder(double amount) throws Exception {
+        RazorpayClient razorpay = new RazorpayClient(razorpayKeyId, razorpayKeySecret);
+
+        JSONObject orderRequest = new JSONObject();
+        orderRequest.put("amount", (int)(amount * 100)); // in paise
+        orderRequest.put("currency", "INR");
+        orderRequest.put("receipt", "txn_" + System.currentTimeMillis());
+        orderRequest.put("payment_capture", 1);
+
+        return razorpay.orders.create(orderRequest);
+    }
+}
